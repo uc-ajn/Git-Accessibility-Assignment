198bf84f36d822ae4bbfa13bc176ac0bd37e3311
Aayush Jain <aayush.jain@ucertify.com>
2022-02-16 09:13:16 +0530

    bugs are resolved

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-document.body.addEventListener('keypress', function(e) {
-    console.log(e)
-    if (e.key == "Enter") {
-      console.log("Enter pressed")
-      submit();
-    } 
-    if (e.key == "Escape") {
-        console.log("Escape pressed")
-        cancel()
-      }
-  });
 
-  function submit(){
-      alert('Ok button pressed')
+document.getElementById("heading").focus(); 
+
+document.body.addEventListener('keypress', function (e) {
+  console.log(e)
+  if (e.key == "k") {
+    console.log("Enter pressed")
+    submit();
   }
+  if (e.key == "c") {
+    console.log("Escape pressed")
+    cancel()
+  }
+});
 
-  function cancel(){
-    alert('Cancel button pressed')
+function submit() {
+  alert('Ok button pressed')
 }
 
-var firstElement = document.getElementById("tutorials")
+function cancel() {
+  alert('Cancel button pressed')
 
-// document.getElementById("tutorials").on('focus', function() {
-//     document.getElementById("cancel").focus();
-//   });
+}
 
-//   $("#cancel").on('focus', function() {
-//     $('#tutorials').focus();
-//   });
+let focusableElements = document.querySelectorAll('a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]');
+focusableElements = Array.prototype.slice.call(focusableElements);
+
+const firstElement = focusableElements[0];
+const lastElement = focusableElements[focusableElements.length - 1];
+
+document.body.addEventListener('keydown', trap);
+
+  function trap(event) {
+    if (event.keyCode === 9) {
+      if (event.shiftKey) {
+        if (document.activeElement === firstElement) {
+          event.preventDefault();
+          lastElement.focus();
+        }
+
+      } else {
+        if (document.activeElement === lastElement) {
+          event.preventDefault();
+          firstElement.focus();
+        }
+      }
+
+    }
+  }
+
+
+
+
+
